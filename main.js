37736fa0533cb29c7f7c15e64131734653ef1f1e
Arela-Cristina <instagrandine@gmail.com>
2024-10-28 15:32:42 +0100

    take elements from DOM

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 // //Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
 // (trovate l’array del team all’interno della cartella in allegato)
+
+
+//container DOM
+const employeerCard = document.getElementById('employeerCard')
+//elementi DOM
+const employeerPicture = document.getElementById('employeerPicture')
+const employeerName = document.getElementById('employeerName')
+const employeerPosition = document.getElementById('employeerPosition')
+const employeerMail = document.getElementById('employeerMail')
+
